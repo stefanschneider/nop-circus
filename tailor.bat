9ee31893adc68e0f438194a49b282f97820a4667
Stefan Schneider <stefan.schneider@uhurusoftware.com>
2014-07-08 14:07:14 +0300

    Change tailor result

--- tailor.bat
+++ tailor.bat
@@ -5,7 +5,7 @@ mkdir tmp\droplet\app
 xcopy app tmp\droplet\app /s /e /i /h
 
 mkdir tmp\result
-echo {"detected_buildpack": "nop", "detected_start_command": ""} > tmp\result\result.json
+echo {"detected_buildpack": "dummy-buildpack", "detected_start_command": "start"} > tmp\result\result.json
 rem echo --- > tmp\result\staging_info.yml
 
 exit /B 0
