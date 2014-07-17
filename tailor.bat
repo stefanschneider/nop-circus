55d6b9f8e3410f9d2fc5f29ac490096344eab45c
sstefan <stefan.schneider@uhurusoftware.com>
2014-07-17 11:50:16 +0300

    update tailor

--- tailor.bat
+++ tailor.bat
@@ -8,7 +8,7 @@ mkdir tmp\cache
 echo nop > tmp\cache\nop
 
 mkdir tmp\result
-echo|set /p={"detected_buildpack": "dummy-buildpack", "detected_start_command": "start"} > tmp\result\result.json
+echo|set /p={"buildpack_key":"52f71429-6589-44ef-bb5c-7e827a0a0af5_52808bb309461b2331e7996304af1be7d037a756", "detected_buildpack": "dummy-buildpack", "detected_start_command": "start"} > tmp\result\result.json
 rem echo --- > tmp\result\staging_info.yml
 
 exit /B 0
