ad1f9932b27baabab7fd0899a62828ccd1e4d422
sstefan <stefan.schneider@uhurusoftware.com>
2014-07-17 09:51:30 +0300

    add some content to tmp cache

--- tailor.bat
+++ tailor.bat
@@ -4,6 +4,9 @@ mkdir tmp\droplet\app
 
 xcopy app tmp\droplet\app /s /e /i /h
 
+mkdir tmp\cache
+echo nop > tmp\cache\nop
+
 mkdir tmp\result
 echo {"detected_buildpack": "dummy-buildpack", "detected_start_command": "start"} > tmp\result\result.json
 rem echo --- > tmp\result\staging_info.yml
