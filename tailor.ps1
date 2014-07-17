45c8b17ca2a035df226775e0ae1c68567d7a33eb
Stefan Schneider <stefan.schneider@uhurusoftware.com>
2014-07-17 12:33:43 +0300

    add ps powershell

---
+++ tailor.ps1
@@ -0,0 +1,22 @@
+$first_buildpack_key = ""
+
+foreach ($arg in $script:args) {
+  if ($arg.StartsWith("-buildpackOrder")){
+      echo $arg
+      echo $arg.TrimStart("-buildpackOrder=")
+      echo $arg.TrimStart("-buildpackOrder=").Split(",")[0]
+      $first_buildpack_key = $arg.TrimStart("-buildpackOrder=").Split(",")[0]
+  }
+}
+
+Write-Host "Running nop tailor"
+mkdir -force tmp\droplet\app > out-null
+
+xcopy app tmp\droplet\app /s /e /i /h
+
+mkdir -force tmp\cache > out-null
+echo nop >> tmp\cache\nop
+
+mkdir -force tmp\result > out-null
+
+Write-Output "{""buildpack_key"": ""$first_buildpack_key"", ""detected_buildpack"": ""dummy-buildpack"", ""detected_start_command"": ""start""}" > tmp\result\result.json
